9eb78486a50d83c991ff70139f40c07c13438d2d
Nabin Islam <sinabin62246@gmail.com>
2023-01-31 19:26:55 +0600

    mongo connection done

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 const port = process.env.PORT || 5000;
 
@@ -9,6 +10,21 @@ const app = express();
 // middlewares
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lqyancf.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+
+const run = () => {
+  try {
+  } finally {
+  }
+};
+
+run().catch(err => console.error(err));
 
 app.get('/', async (req, res) => {
   res.send('<h1>Redux blog app server is running</h1>');
